34a2bb3c724c892e244413c5cc973ac00077f75c
moki-daniel <mokidnl@gmail.com>
2023-05-04 15:38:36 +0300

    implemented new account function

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg')
 
+//connect to the smartbankdb
 const client = new Client({
     host: 'localhost',
     user: 'postgres',
@@ -15,3 +16,15 @@ client.connect(err => {
     }
     console.log(`\n ✅ Connected Successfully`)
 })
+
+//create a new account function
+
+const createNewAccount = ({ account_id, account_name, balance }, onCreate = undefined) => {
+    client.query(`insert into account values ($1, $2, $3)`, [account_id, account_name, balance], (err, res) => {
+        if (err) console.log(`\n ❌ Problem In Creating the Customer`)
+        else {
+            console.log(`\n ✅ New Customer Created Successfully`)
+            if (onCreate) onCreate(`✅ New Customer Created Successfully`)
+        }
+    })
+}
